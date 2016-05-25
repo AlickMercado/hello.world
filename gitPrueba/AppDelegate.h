6ff49ec72f228116cda0ed30f69ca9b40a2e6190
Alick Mercado <alickmercado@macbook-pro-de-alick.local>
2016-05-25 00:26:05 -0500

    Initial Commit

---
+++ gitPrueba/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitPrueba
+//
+//  Created by Alick Mercado on 25/05/16.
+//  Copyright © 2016 Alick Mercado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitPrueba/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gitPrueba
+//
+//  Created by Alick Mercado on 25/05/16.
+//  Copyright © 2016 Alick Mercado. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
